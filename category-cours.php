55b1f6837a06b187354388b65631053bf08412b4
Rachel Boulay <boulayrachel.52@gmail.com>
2019-03-01 16:48:46 -0500

    ajout de fonction de triage

--- category-cours.php
+++ category-cours.php
@@ -20,7 +20,6 @@ get_header();
 			<header class="page-header">
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					query_posts('posts_per_page=-1');
 				?>
 			</header><!-- .page-header -->
 			<div class="liste">
--- template-parts/content/content-titre-cours.php
+++ template-parts/content/content-titre-cours.php
@@ -8,7 +8,6 @@
  * @subpackage Twenty_Nineteen
  * @since 1.0.0
  */
-
 ?>
 <?php
 	the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
